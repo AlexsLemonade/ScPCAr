a14853537440dad49981b9c9b936d22e26b3f694
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2025-09-02 16:40:29 -0400

    add function to get auth token
    update scpca_request to handle the body too

--- R/api.R
+++ R/api.R
@@ -5,15 +5,17 @@ USER_AGENT <- "ScPCA R API Client" # nolint
 #' Base request object for ScPCA API
 #'
 #' @param resource API resource to query, e.g. "projects"
+#' @param body optional named list to include as JSON body in the request
 #' @param auth_token optional API authentication token
 #' @param ... additional query parameters to include in the request
 #'
 #' @import httr2
 #'
 #' @returns a httr2 request object (invisibly)
-scpca_request <- function(resource, auth_token = "", ...) {
+scpca_request <- function(resource, body = list(), auth_token = "", ...) {
   stopifnot(
-    "Resource must be a non-empty string" = is.character(resource) && nchar(resource) > 0
+    "resource must be a non-empty string" = is.character(resource) && nchar(resource) > 0,
+    "body must be a named list" = is.list(body) && (length(body) == 0 || !is.null(names(body)))
   )
 
   call <- httr2::request(API_BASE) |>
@@ -23,6 +25,10 @@ scpca_request <- function(resource, auth_token = "", ...) {
 
   if (nchar(auth_token) > 0) {
     call <- call |> req_headers_redacted(`api-key` = auth_token)
+  }
+
+  if (length(body) > 0) {
+    call <- call |> req_body_json(body)
   }
 
   invisible(call)
---
+++ R/auth.R
@@ -0,0 +1,23 @@
+#' Get an authorization token from the ScPCA API
+#'
+#' @param email The user's email address
+#' @param agree A logical indicating whether the user agrees to the terms of service
+#'
+#' @returns A string containing the authorization token (invisibly)
+#'
+#' @import httr2
+#'
+#' @export
+get_token <- function(email, agree = FALSE) {
+  stopifnot(
+    "You must agree to the terms of service to get a token." = agree,
+    "Invalid email address" = grepl("^[^@]+@[^@]+\\.[^@]+$", email)
+  )
+
+  req <- scpca_request("tokens", body = list(email = email, is_activated = agree))
+
+  response <- req_perform(req) |>
+    resp_body_json(simplifyVector = TRUE)
+
+  invisible(response$id)
+}
