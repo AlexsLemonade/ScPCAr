d5386e99a0d6f5e1a9b29120f77715b065d56740
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2025-09-03 17:55:13 -0400

    add simplify option to project list

--- R/projects.R
+++ R/projects.R
@@ -1,11 +1,13 @@
 #' Get data frame of all ScPCA projects
 #'
+#' @param simplify A logical indicating whether to simplify the resulting data frame by removing list columns. Default is TRUE.
+#'
 #' @returns a data frame of project information from the ScPCA API.
 #'
 #' @import httr2
 #'
 #' @export
-scpca_projects <- function() {
+scpca_projects <- function(simplify = TRUE) {
   responses <- scpca_request("projects") |>
     req_perform_iterative(iterate_scpca)
 
@@ -14,6 +16,10 @@ scpca_projects <- function() {
       resp_body_json(resp, simplifyVector = TRUE)$results |>
         as.data.frame()
     })
+
+  if (simplify) {
+    df <- df |> dplyr::select(where(\(col) !is.list(col)))
+  }
 
   df
 }
