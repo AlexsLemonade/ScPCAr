2166e5eb2bc330beff8028176bd427d040e290c7
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2025-10-17 14:08:42 -0400

    Add shbang/comments to precompile script

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,3 +1,11 @@
+#!/usr/bin/env Rscript
+
+# This script renders vignettes that depend on data downloads from the SCPCA API.
+# Run this script prior to building the package to pre-compile vignettes.
+#
+# Usage: Rscript vignettes/precompile.R
+
+
 devtools::load_all()
 
 # Pre-compiled vignettes that depend on data downloads
