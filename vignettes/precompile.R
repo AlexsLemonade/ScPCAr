a36b9524181c199584022a1900d60f6be8cd69c8
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2025-10-20 10:32:57 -0400

    Small QOL figure placement updates

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -7,10 +7,7 @@
 
 
 devtools::load_all()
+setwd(here::here("vignettes"))
 
 # Pre-compiled vignettes that depend on data downloads
-knitr::knit(here::here("vignettes/ScPCAr.Rmd.orig"), output = here::here("vignettes/ScPCAr.Rmd"))
-
-# move figures
-unlink(here::here("vignettes/figure"), recursive = TRUE)
-file.rename(here::here("figure"), here::here("vignettes/figure"))
+knitr::knit("ScPCAr.Rmd.orig", output = "ScPCAr.Rmd")
