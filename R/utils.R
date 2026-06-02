b258e7bc4c44fe2b9e832f3f9025554d0ca7fa88
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-06-02 10:25:08 -0400

    rename destination check, clarify uuid check

--- R/utils.R
+++ R/utils.R
@@ -10,14 +10,14 @@ is_testing <- function() {
   identical(Sys.getenv("TESTTHAT"), "true")
 }
 
-#' Check whether a value is a single UUID string
+#' Check whether a value has UUID string format
 #'
-#' UUIDs look like "123e4567-e89b-12d3-a456-426614174000" and are used for both
+#' UUIDs look like "123e4567-889b-43d2-9999-123456789abc" and are used for both
 #' dataset IDs and authorization tokens in the ScPCA API.
 #'
 #' @param x value to test
 #'
-#' @return Logical indicating whether `x` is a length-1 UUID string
+#' @return Logical indicating whether `x` is a length-1 UUID-formatted string
 #' @noRd
 is_uuid <- function(x) {
   uuid_pattern <- "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
