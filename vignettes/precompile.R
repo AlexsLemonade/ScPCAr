2d9c11b42ceeb235ab0e01afeec6284601397892
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-06-09 09:33:41 -0400

    Note AUTH token requirement in precompile script

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -3,8 +3,18 @@
 # This script renders vignettes that depend on data downloads from the SCPCA API.
 # Run this script prior to building the package to pre-compile vignettes.
 #
+# Set the SCPCA_AUTH_TOKEN environment variable to a valid token before running this script.
+#
 # Usage: Rscript vignettes/precompile.R
 
+uuid_regex <- "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+
+stopifnot(
+  "SCPCA_AUTH_TOKEN environment variable must be set and a UUID" = grepl(
+    uuid_regex,
+    Sys.getenv("SCPCA_AUTH_TOKEN")
+  )
+)
 
 devtools::load_all()
 setwd(here::here("vignettes"))
