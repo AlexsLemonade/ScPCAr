b637ac3928c65280892c7aba7bdc5f8de4a4e453
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-06-11 14:16:01 -0400

    update precompile script  for new path and getting auth

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -7,17 +7,12 @@
 #
 # Usage: Rscript vignettes/precompile.R
 
-uuid_regex <- "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
-
-stopifnot(
-  "SCPCA_AUTH_TOKEN environment variable must be set and a UUID" = grepl(
-    uuid_regex,
-    Sys.getenv("SCPCA_AUTH_TOKEN")
-  )
-)
-
 devtools::load_all()
 setwd(here::here("vignettes"))
 
+# Get auth token and set it in the environment for knitr to use
+ScPCAr::get_auth(email = "scpca@ccdatalab.org", agree = TRUE)
+
+
 # Pre-compiled vignettes that depend on data downloads
-knitr::knit("ScPCAr.Rmd.orig", output = "ScPCAr.Rmd")
+knitr::knit("uncompiled/ScPCAr_pre.Rmd", output = "ScPCAr.Rmd")
