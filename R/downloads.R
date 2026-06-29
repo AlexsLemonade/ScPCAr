06ff8a8c5bfc1d82e37292b6a70e4f813c723b6f
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-06-29 15:52:32 -0400

    Add check for interactive sessions

--- R/downloads.R
+++ R/downloads.R
@@ -625,10 +625,14 @@ await_dataset_processing <- function(
   status <- get_dataset_status(dataset_id, auth_token = auth_token)
 
   if (!quiet) {
-    cli::cli_progress_bar(
-      format = "{cli::pb_spin} Waiting for dataset {dataset_id} [{status}] {cli::pb_elapsed}",
-      clear = FALSE
-    )
+    if (interactive()) {
+      cli::cli_progress_bar(
+        format = "{cli::pb_spin} Waiting for dataset {dataset_id} [{status}] {cli::pb_elapsed}",
+        clear = FALSE
+      )
+    } else {
+      cli::cli_alert_info("Waiting for dataset {dataset_id} to finish processing...")
+    }
   }
 
   repeat {
@@ -668,7 +672,7 @@ await_dataset_processing <- function(
       )
     }
 
-    if (!quiet) {
+    if (!quiet && interactive()) {
       next_loop <- Sys.time() + poll_interval * 60
       # keep the progress spinner updating every half second until the next poll
       while (Sys.time() < next_loop) {
